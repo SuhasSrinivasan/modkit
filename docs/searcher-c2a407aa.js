6da7d7d36e2cd8bcc8e36ae4de9452315c407431
Art Rand <art.rand@nanoporetech.com>
2025-11-30 08:31:09 -0800

    chore(docs): build docs

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b27bf96e.js',
+                path_to_root + 'searchindex-837351bc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
